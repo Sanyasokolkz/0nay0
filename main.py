9ca94c779f5e798d8e9daf437d42fb2456dc902d
Sanyasokolkz <Sanyasokolkz@gmail.com>
2025-01-21 03:22:27 +0500

    Добавлена библиотека pytz

--- main.py
+++ main.py
@@ -2,10 +2,13 @@ import random
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 from datetime import datetime
-import pytz  # Библиотека для работы с часовыми поясами
+import pytz
+import os
 
-TOKEN = '7885324267:AAHFISc1gkB7BCLXllYhFNG3bZIUeU9g4a0'
+# Токен Telegram-бота (указывается через переменные окружения для безопасности)
+TOKEN = os.getenv("TOKEN")
 FIXED_PRICE = '120₸'
+PORT = int(os.getenv("PORT", "8443"))  # Порт по умолчанию 8443
 
 # Генерация случайного гос. номера
 def generate_random_license():
@@ -51,15 +54,23 @@ async def bus_info(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     # Удаление сообщения пользователя
     await context.bot.delete_message(chat_id=update.message.chat_id, message_id=update.message.message_id)
 
+# Основная функция
 def main():
+    # Создаем приложение Telegram
     application = Application.builder().token(TOKEN).build()
 
-    # Добавляем обработчики команд и сообщений
+    # Добавляем обработчики
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, bus_info))
 
-    # Запуск бота
-    application.run_polling()
+    # Настройка Webhook
+    webhook_url = f"https://{os.getenv('RAILWAY_STATIC_URL')}/{TOKEN}"  # Используем Railway домен
+    application.run_webhook(
+        listen="0.0.0.0",
+        port=PORT,
+        url_path=TOKEN,
+        webhook_url=webhook_url,
+    )
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
